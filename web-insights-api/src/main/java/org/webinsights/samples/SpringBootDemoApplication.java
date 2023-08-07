a51a53046bbd60a38f0b7f057b12b6b2e8e50ec0
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-08-08 00:26:10 +0200

    rename folders, update readme

--- web-insights-api/src/main/java/org/webinsights/samples/SpringBootDemoApplication.java
+++ web-insights-api/src/main/java/org/webinsights/samples/SpringBootDemoApplication.java
@@ -1,4 +1,4 @@
-package com.web_insights.samples;
+package org.webinsights.samples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
