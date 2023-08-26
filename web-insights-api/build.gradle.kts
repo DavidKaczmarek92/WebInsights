a087e164f7d1bfe959600a589ac39958783d8252
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-08-27 00:35:36 +0200

    rebuild after files change

--- web-insights-api/build.gradle.kts
+++ web-insights-api/build.gradle.kts
@@ -1,11 +1,10 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 plugins {
     id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.3"
     java
 }
-
-version = "1.0.2"
-group = "org.gradle.samples"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -22,6 +21,7 @@ dependencies {
     implementation("org.postgresql:postgresql")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(mapOf("group" to "org.junit.vintage", "module" to "junit-vintage-engine"))
@@ -31,4 +31,8 @@ dependencies {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.named<BootRun>("bootRun") {
+    sourceResources(sourceSets["main"])
 }
