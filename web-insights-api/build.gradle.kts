456ba3c681867ff0336b865fffd55545020338b4
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-08-26 02:20:51 +0200

    add spring boot data jpa

--- web-insights-api/build.gradle.kts
+++ web-insights-api/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.springframework.boot") version("3.1.2")
-    id("io.spring.dependency-management") version("1.1.0")
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
     java
 }
 
@@ -16,12 +16,13 @@ repositories {
 }
 
 dependencies {
-  implementation(platform("org.springframework.boot:spring-boot-dependencies:3.1.2"))
+  implementation(platform("org.springframework.boot:spring-boot-dependencies:3.1.3"))
 
-    implementation("org.springframework.boot:spring-boot-starter")
+
+    implementation("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(mapOf("group" to "org.junit.vintage", "module" to "junit-vintage-engine"))
     }
