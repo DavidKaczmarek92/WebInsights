a8a6917d1d223066d9062051efe93dba4c9c829a
David <33669220+DavidKaczmarek92@users.noreply.github.com>
2023-08-29 16:06:35 +0200

    Setup database (#44)
    What
    - add  and configure`spring-boot-starter-data-jpa` gradle plugin
    - add docker-compose file for database
    - add and configure `spring-boot-devtools` gradle plugin

--- web-insights-api/build.gradle.kts
+++ web-insights-api/build.gradle.kts
@@ -1,10 +1,10 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 plugins {
-    id("org.springframework.boot") version("3.1.2")
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
     java
 }
-
-version = "1.0.2"
-group = "org.gradle.samples"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -15,9 +15,14 @@ repositories {
 }
 
 dependencies {
-  implementation(platform("org.springframework.boot:spring-boot-dependencies:3.1.2"))
+  implementation(platform("org.springframework.boot:spring-boot-dependencies:3.1.3"))
 
-    implementation("org.springframework.boot:spring-boot-starter")
+
+    implementation("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(mapOf("group" to "org.junit.vintage", "module" to "junit-vintage-engine"))
     }
@@ -26,4 +31,8 @@ dependencies {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.named<BootRun>("bootRun") {
+    sourceResources(sourceSets["main"])
 }
