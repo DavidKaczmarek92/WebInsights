eeb564434500379d34fef26c5d7dbda3fc4cb361
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-09-02 23:04:42 +0200

    rename folder name

--- web-insights-app/app/components/primitives/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Button.module.css";
-
-type ButtonProps = {
-  text?: string;
-};
-
-export function Button({ text }: ButtonProps) {
-  return <button className={styles.base}>{text}</button>;
-}
