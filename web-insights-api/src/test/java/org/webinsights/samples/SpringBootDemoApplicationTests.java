a93f3f701d868b513e7babc3e82c7c77e0f60c25
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-09-11 00:08:41 +0200

    fix test

--- web-insights-api/src/test/java/org/webinsights/samples/SpringBootDemoApplicationTests.java
+++ web-insights-api/src/test/java/org/webinsights/samples/SpringBootDemoApplicationTests.java
@@ -1,14 +1,11 @@
 package org.webinsights.samples;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
 
   @Test
-  void contextLoads() {
-    assertTrue(false);
-  }
+  void contextLoads() {}
 }
