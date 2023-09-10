d2a2bacf8bb4f1afe16ddca38ea97dc0e79fa8df
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-09-11 00:04:26 +0200

    fail example

--- web-insights-api/src/test/java/org/webinsights/samples/SpringBootDemoApplicationTests.java
+++ web-insights-api/src/test/java/org/webinsights/samples/SpringBootDemoApplicationTests.java
@@ -1,11 +1,14 @@
 package org.webinsights.samples;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
 
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    assertTrue(false);
+  }
 }
