7c4abe5acee502f801edb5e35d72b4e48099493f
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-09-10 11:23:04 +0200

    add basic styles for Typography

--- web-insights-app/app/components/primitive/Typography/Typography.stories.tsx
+++ web-insights-app/app/components/primitive/Typography/Typography.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import type { AllowedTags } from "./Typography";
 import { Typography as TypographyComponent } from "./Typography";
 
 const meta: Meta<typeof TypographyComponent> = {
@@ -8,16 +9,44 @@ const meta: Meta<typeof TypographyComponent> = {
 
 type Story = StoryObj<typeof TypographyComponent>;
 
-const children = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+const tags: Array<AllowedTags> = [
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "h6",
+  "p",
+  "label",
+  "span",
+];
 
 export const Typography: Story = {
-  args: {
-    children,
-  },
   render: () => {
     return (
       <>
-        <TypographyComponent>{children}</TypographyComponent>
+        {tags.map((tag) => (
+          <div key={tag}>
+            <TypographyComponent as="h3">Tag {tag}</TypographyComponent>
+            <div>
+              <TypographyComponent as={tag} variant="bold">
+                {text}
+              </TypographyComponent>
+            </div>
+            <div>
+              <TypographyComponent as={tag} variant="normal">
+                {text}
+              </TypographyComponent>
+            </div>
+            <div>
+              <TypographyComponent as={tag} variant="light">
+                {text}
+              </TypographyComponent>
+            </div>
+          </div>
+        ))}
       </>
     );
   },
--- web-insights-app/app/components/primitive/Typography/Typography.tsx
+++ web-insights-app/app/components/primitive/Typography/Typography.tsx
@@ -4,13 +4,31 @@ import styles from "./Typography.module.css";
 
 const cx = classNames.bind(styles);
 
+export type AllowedTags =
+  | "label"
+  | "span"
+  | "p"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6";
+
 type TypographyProps = {
+  as?: AllowedTags;
+  className?: string;
+  variant?: "bold" | "normal" | "light";
   children: ReactNode;
-  as?: "label" | "span" | "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 };
 
-export function Typography({ as: Tag = "p", children }: TypographyProps) {
-  const classes = cx("basic", Tag);
+export function Typography({
+  as: Tag = "p",
+  variant = "normal",
+  className,
+  children,
+}: TypographyProps) {
+  const classes = cx("basic", Tag, variant, className);
 
   return <Tag className={classes}>{children}</Tag>;
 }
