bc70c5bc584531f540d04c25fd7aa25faa12c55c
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-11-26 22:16:41 +0100

    lint

--- web-insights-app/app/routes/_index.tsx
+++ web-insights-app/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { V2_MetaFunction } from "@remix-run/node";
-import { Form, Link, useLoaderData } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [
