1dc386fa60124112c5a006c09f8005c24ce4b5d1
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-12-12 21:39:48 +0100

    refactor

--- web-insights-api/src/main/java/org/webinsights/samples/SecurityConfig.java
+++ web-insights-api/src/main/java/org/webinsights/samples/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.webinsights.samples;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -11,9 +13,6 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -27,8 +26,9 @@ public class SecurityConfig {
     public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
-                    .anyRequest().authenticated()
-                )
+                .anyRequest()
+                .authenticated()
+            )
             .oauth2Login(withDefaults());
 
         return http.build();
@@ -37,11 +37,13 @@ public class SecurityConfig {
     @Order(2)
     @Bean
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers(new AntPathRequestMatcher("*"))
-                .hasRole("USER")
-                .anyRequest()
-                .authenticated();
+        http.authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("*")
+            .hasRole("USER")
+            .anyRequest()
+            .authenticated()
+        );
+
         http.oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
 
         return http.build();
