26a354509dabf1989f5ba49990d8760330696c51
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2023-12-18 23:18:13 +0100

    do not return json if user is authenticate

--- web-insights-app/app/routes/auth.keycloak.tsx
+++ web-insights-app/app/routes/auth.keycloak.tsx
@@ -2,7 +2,9 @@ import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { authenticator } from "~/utils/auth";
 
 export const loader: LoaderFunction = ({ request }) => {
-  return authenticator.authenticate("keycloak", request);
+  return authenticator.authenticate("keycloak", request, {
+    successRedirect: "/",
+  });
 };
 
 export const action: ActionFunction = async ({ request }) => {
