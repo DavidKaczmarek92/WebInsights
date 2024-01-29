744c2154041d59c78aee268be5c412822c8ffa14
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2024-01-29 21:40:13 +0100

    create no security config and some endpoints

--- web-insights-api/src/main/java/org/webinsights/api/workflow/WorkflowController.java
+++ web-insights-api/src/main/java/org/webinsights/api/workflow/WorkflowController.java
@@ -1,8 +1,11 @@
 package org.webinsights.api.workflow;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/workflows")
@@ -16,5 +19,28 @@ public class WorkflowController {
   @GetMapping
   public List<Workflow> getAllWorkflows() {
     return workflowService.findAllWorkflows();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Workflow> getWorkflowBy(@PathVariable Long id) {
+    Optional<Workflow> workflow = workflowService.findWorkflowById(id);
+
+    return workflow.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @PostMapping
+  public Workflow createWorkflow(@RequestBody Workflow workflow) {
+    return workflowService.saveWorkflow(workflow);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> deleteWorkflow(@PathVariable Long id) {
+    try {
+      workflowService.deleteWorkflow(id);
+
+      return ResponseEntity.ok().build();
+    } catch (EmptyResultDataAccessException e) {
+      return ResponseEntity.notFound().build();
+    }
   }
 }
