2ff093980477f4d522209ae229e6964498e5f64e
David <33669220+DavidKaczmarek92@users.noreply.github.com>
2024-02-14 21:13:12 +0100

    rollback app changes (#83)

--- web-insights-app/app/routes/dashboard.tsx
+++ web-insights-app/app/routes/dashboard.tsx
@@ -1,21 +1,12 @@
 import type { LoaderFunction } from "@remix-run/node";
-import { Form, Link, useLoaderData } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 import { isAuthenticated } from "~/utils/auth";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  await isAuthenticated(request);
-
-  const response = await fetch("http://localhost:8080/workflows");
-  const data = await response.json();
-
-  return { data };
+  return await isAuthenticated(request);
 };
 
 export default function Dashboard() {
-  const data = useLoaderData();
-
-  console.log(data);
-
   return (
     <div>
       <h1>dashboard</h1>
