9df7fe91e23693242d5ab460bcd37181e04a55b0
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2024-02-24 21:37:43 +0100

    first attempt to fix keycloak integration

--- web-insights-api/src/main/java/org/webinsights/api/config/SecurityConfig.java
+++ web-insights-api/src/main/java/org/webinsights/api/config/SecurityConfig.java
@@ -1,49 +1,31 @@
 package org.webinsights.api.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-  @Bean
-  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-  }
 
-  @Order(1)
+  private final JwtAuthConverter jwtAuthConverter;
+
   @Bean
-  public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-        .oauth2Login(withDefaults());
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated();
+
+    http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
+
+    http.sessionManagement().sessionCreationPolicy(STATELESS);
 
     return http.build();
-  }
-
-  @Order(2)
-  @Bean
-  public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(
-        authorize -> authorize.requestMatchers("*").hasRole("USER").anyRequest().authenticated());
-
-    http.oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
-
-    return http.build();
-  }
-
-  @Bean
-  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-    return http.getSharedObject(AuthenticationManagerBuilder.class).build();
   }
 }
--- web-insights-app/app/routes/dashboard.tsx
+++ web-insights-app/app/routes/dashboard.tsx
@@ -1,12 +1,41 @@
 import type { LoaderFunction } from "@remix-run/node";
-import { Form, Link } from "@remix-run/react";
-import { isAuthenticated } from "~/utils/auth";
+import { json, redirect } from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { isAuthenticated, sessionStorage } from "~/utils/auth";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  return await isAuthenticated(request);
+  const user = await isAuthenticated(request);
+
+  if (user) {
+    const res = await fetch("http://localhost:8080/workflows", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    });
+
+    if (res.status === 401) {
+      const currentSession = await sessionStorage.getSession(
+        request.headers.get("_session"),
+      );
+
+      return redirect("/auth/keycloak", {
+        headers: {
+          "Set-Cookie": await sessionStorage.destroySession(currentSession),
+        },
+      });
+    }
+
+    const workflows: Array<{ title: string }> = await res.json();
+
+    return json({ workflows });
+  }
 };
 
 export default function Dashboard() {
+  const data: { workflows: [{ title: string }] } =
+    useLoaderData<typeof loader>();
+
   return (
     <div>
       <h1>dashboard</h1>
@@ -14,6 +43,9 @@ export default function Dashboard() {
         <button>Logout</button>
       </Form>
       <Link to="/">Go to home page</Link>
+      {data.workflows.map(({ title }) => (
+        <h2 key={title}>Workflow title: {title}</h2>
+      ))}
     </div>
   );
 }
