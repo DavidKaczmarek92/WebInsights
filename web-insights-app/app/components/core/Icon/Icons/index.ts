fdae000e2e66f931dba3cb85d3a0a9af6b8e4b4e
David <33669220+DavidKaczmarek92@users.noreply.github.com>
2024-04-15 21:02:00 +0200

    fix typo for svg component (#93)

--- web-insights-app/app/components/core/Icon/Icons/index.ts
+++ web-insights-app/app/components/core/Icon/Icons/index.ts
@@ -1,3 +1,3 @@
 export { default as Bell } from "./Bell";
 export { default as Logout } from "./Logout";
-export { default as Worklfow } from "./Worklfow";
+export { default as Workflow } from "./Workflow";
