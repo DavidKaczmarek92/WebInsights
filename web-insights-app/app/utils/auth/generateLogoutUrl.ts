16db4a8bf79c9e513be1fd1f7f3bdae4fe31e826
David <33669220+DavidKaczmarek92@users.noreply.github.com>
2024-07-29 06:38:21 +0200

    Install angular (#102)

--- web-insights-app/app/utils/auth/generateLogoutUrl.ts
+++
@@ -1,7 +0,0 @@
-import { isProductionEnv } from "~/utils/isProductionEnv";
-
-export function generateLogoutUrl() {
-  return `${isProductionEnv() ? "https" : "http"}://${
-    process.env.KEYCLOAK_DOMAIN
-  }/realms/WebInsights/protocol/openid-connect/logout`;
-}
