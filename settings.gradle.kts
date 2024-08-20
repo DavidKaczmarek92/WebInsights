77681f3c1a1460da440d9a09dd93cf9f77cc463d
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2024-08-20 21:36:07 +0200

    feat: add conventional commits

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,13 @@
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.6"
+}
+
 rootProject.name = "WebInsights"
 
 include("web-insights-api")
 include("web-insights-docker")
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks()
+}
