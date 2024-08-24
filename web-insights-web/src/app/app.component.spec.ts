599ad12a1c8bcaa43b39e61dc03bb3f7f33b06fe
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2024-08-24 07:45:50 +0200

    test: github action karma tests

--- web-insights-web/src/app/app.component.spec.ts
+++ web-insights-web/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Web Insights');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Web Insight');
   });
 });
