15ca170605aab5fea540b44f52f9f84c6851466b
DavidKaczmarek92 <d.kaczmarek29@gmail.com>
2024-12-01 09:32:45 +0100

    feat: save current work

--- web-insights-web/src/app/app.component.ts
+++ web-insights-web/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'sp-root',
-  standalone: true,
+  selector: 'sp-app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  standalone: true,
 })
 export class AppComponent {
-  title = 'web-insights-web';
+  title = 'Spectrana';
 }
--- web-insights-web/src/app/app.config.ts
+++ web-insights-web/src/app/app.config.ts
@@ -1,11 +1,65 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  Provider,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { KeycloakBearerInterceptor, KeycloakService } from 'keycloak-angular';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
 
+// TODO: https://medium.com/@ragabon/create-multiple-environment-ts-in-angular-using-env-and-automate-using-node-js-933503fca752
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      // Configuration details for Keycloak
+      config: {
+        url: 'http://localhost:8888', // URL of the Keycloak server
+        realm: 'WebInsights', // Realm to be used in Keycloak
+        clientId: 'insider', // Client ID for the application in Keycloak
+      },
+      // Options for Keycloak initialization
+      initOptions: {
+        onLoad: 'check-sso', // Action to take on load
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html', // URI for silent SSO checks
+      },
+      // Enables Bearer interceptor
+      enableBearerInterceptor: true,
+      // Prefix for the Bearer token
+      bearerPrefix: 'Bearer',
+      // URLs excluded from Bearer token addition (empty by default)
+      bearerExcludedUrls: ['/assets', '/clients/public'],
+    });
+}
+
+const KeycloakBearerInterceptorProvider: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: KeycloakBearerInterceptor,
+  multi: true,
+};
+
+const KeycloakInitializerProvider: Provider = {
+  provide: APP_INITIALIZER,
+  useFactory: initializeKeycloak,
+  multi: true,
+  deps: [KeycloakService],
+};
+
+// https://github.com/mauriciovigolo/keycloak-angular/issues/384#issuecomment-1895845160
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptorsFromDi()),
+    KeycloakInitializerProvider,
+    KeycloakBearerInterceptorProvider,
+    KeycloakService,
     provideRouter(routes),
   ],
 };
