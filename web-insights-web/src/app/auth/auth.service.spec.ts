d289285abc36665232bf7c7b8893de5438145661
David <33669220+DavidKaczmarek92@users.noreply.github.com>
2025-01-04 23:29:37 +0100

    feat: create simple auth service (#145)
    # Why ?
    Provide simple logout feature via Keycloak
    # What ?
    1. Create `AuthService` which is fasade for `KeycloakService` which we
    will use in the others services.
    2. Add `logout` method to `AuthService`.

---
+++ web-insights-web/src/app/auth/auth.service.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed } from '@angular/core/testing';
+
+import { KeycloakService } from 'keycloak-angular';
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  let keycloakServiceSpy: jasmine.SpyObj<KeycloakService>;
+
+  beforeEach(() => {
+    keycloakServiceSpy = jasmine.createSpyObj('KeycloakService', {
+      logout: Promise.resolve(),
+    });
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: KeycloakService,
+          useValue: keycloakServiceSpy,
+        },
+      ],
+    });
+
+    authService = TestBed.inject(AuthService);
+  });
+
+  it('should call logout from KeycloakService', () => {
+    authService.logout();
+
+    expect(keycloakServiceSpy.logout).toHaveBeenCalled();
+    expect(authService.isLoading()).toBeTruthy();
+  });
+
+  it('should set isLoading to false when logout fails', async () => {
+    keycloakServiceSpy.logout.and.returnValues(Promise.reject('error'));
+
+    try {
+      await authService.logout();
+    } catch (e) {
+      expect(e).toBe('error');
+      expect(authService.isLoading()).toBeFalsy();
+    }
+  });
+});
