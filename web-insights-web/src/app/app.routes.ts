31bc9b39ef2945e0a669f9dd08f0d68c30dd1ff7
David <d.kaczmarek29@gmail.com>
2025-04-29 22:46:48 +0200

    feat: create primitive home-page component only for testing

--- web-insights-web/src/app/app.routes.ts
+++ web-insights-web/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { ExampleComponent } from './example/example.component';
 import { AuthGuard } from './guard/auth.guard';
+import { HomePageComponent } from './home-page/home-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ExampleComponent,
+    component: HomePageComponent,
     pathMatch: 'full',
     canActivate: [AuthGuard],
   },
